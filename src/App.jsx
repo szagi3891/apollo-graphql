d21201b4ac35827bd8207aa8f0b3dc9766c68973
Grzegorz Szeliga <szeligagrzegorz@gmail.com>
2018-05-20 07:30:24 +0200

    query

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,38 @@
 //@flow
 import React from 'react';
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
 
 type PropsType = {|
 |};
 
+const query = gql`
+{
+    rates(currency: "USD") {
+        currency
+        rate
+    }
+}
+`;
+
 export class App extends React.PureComponent<PropsType> {
     render() {
-        return (<div>App ...</div>);
+        return (
+            <div>
+                <Query query={query}>
+                    {({ loading, error, data }) => {
+                        if (loading) return <p>Loading...</p>;
+                        if (error) return <p>Error :(</p>;
+
+                        return data.rates.map(({ currency, rate }) => (
+                            <div key={currency}>
+                            <p>{`${currency}: ${rate}`}</p>
+                            </div>
+                        ));
+                    }}
+                </Query>
+                App ...
+            </div>
+        );
     }
 }
--- src/index-react.js
+++ src/index-react.js
@@ -3,10 +3,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
 
+
+import ApolloClient from "apollo-boost";
+const client = new ApolloClient({
+  uri: "https://w5xlvm3vzz.lp.gql.zone/graphql"
+});
+
+import { ApolloProvider } from "react-apollo";
+
 const root = document.getElementById('root');
 
 if (root) {
-    ReactDOM.render(<App />, root);
+    ReactDOM.render((
+        <ApolloProvider client={client}>
+            <App />
+        </ApolloProvider>
+    ), root);
 } else {
     console.error('App startup: #root element not found');
 }
