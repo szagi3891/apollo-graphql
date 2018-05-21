ee3d7460a60f64a9364caf54652624a280a9c5f8
Grzegorz Szeliga <grzegorz.szeliga@joivy.com>
2018-05-21 08:31:04 +0200

    poprawienie budowania

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@ set -e
 #https://github.com/reactivestack/parcel-react-ssr/blob/master/package.json
 
 #build client
-./node_modules/.bin/cross-env BABEL_ENV=client parcel build src/index.js -d dist/static --public-url /static
+./node_modules/.bin/cross-env BABEL_ENV=client ./node_modules/.bin/parcel build src/index.js -d dist/static --public-url /static
 
 #build server
-./node_modules/.bin/cross-env BABEL_ENV=server parcel build src/server.js -d dist/static --public-url /static --target=node10
+./node_modules/.bin/cross-env BABEL_ENV=server ./node_modules/.bin/parcel build src/server.js -d dist/static --public-url /static --target=node10
 
 mv dist/static/server.js dist/
 mv dist/static/server.map dist/
